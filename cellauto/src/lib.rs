febf7d067710d5d145fd4beacc26da66b3a34e5f
obizip <ct4kuto@gmail.com>
2024-07-23 22:34:28 +0900

    Add function to calculate cellular automaton

--- cellauto/src/lib.rs
+++ cellauto/src/lib.rs
@@ -1,10 +1,34 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn calc_cellauto(stride: usize) -> Vec<usize> {
-    let data = vec![
-        1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1,
-    ];
+pub fn calc_cellauto(rule: usize, stride: usize) -> Vec<usize> {
+    let rule_vec: Vec<usize> = format!("{:08b}", rule)
+        .chars()
+        .map(|c| c.to_digit(10).expect("should be integer") as usize)
+        .rev()
+        .collect();
+    let mut cellmap = vec![0; stride * stride];
+    // let firstline = vec![1, 1, 0, 1, 1];
+    cellmap[stride / 2 - 1] = 1;
 
-    data
+    for row in 1..stride {
+        for col in 0..stride {
+            let left = if col == 0 {
+                cellmap[(row - 1) * stride + stride - 1]
+            } else {
+                cellmap[(row - 1) * stride + col - 1]
+            };
+            let middle = cellmap[(row - 1) * stride + col];
+            let right = if col == stride - 1 {
+                cellmap[(row - 1) * stride + 0]
+            } else {
+                cellmap[(row - 1) * stride + col + 1]
+            };
+
+            let rule_idx = left * 4 + middle * 2 + right;
+            cellmap[row * stride + col] = rule_vec[rule_idx];
+        }
+    }
+
+    cellmap
 }
