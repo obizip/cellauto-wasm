d0b42477da9cd02ffe650f2448be6c7169edb84a
obizip <ct4kuto@gmail.com>
2024-07-23 19:15:31 +0900

    Add simple function

--- cellauto/src/lib.rs
+++ cellauto/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use wasm_bindgen::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+pub fn calc_cellauto(stride: usize) -> Vec<usize> {
+    let data = vec![
+        1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1,
+    ];
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    data
 }
